a53916152c5b04a5c7213695b1ce1e5ecadfa63f
kiptoo haron <kiptooharon.hk@gmail.com>
2023-10-21 17:53:25 -0400

    fixed pint opcode to do clean up on failure

--- pint.c
+++ pint.c
@@ -9,6 +9,7 @@ void pint(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		free_exit(stack);
 		exit(EXIT_FAILURE);
 	}
 	fprintf(stdout, "%d\n", (*stack)->n);
