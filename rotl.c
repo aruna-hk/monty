55ce396a1062be8ec0a76c69d0b7fd27540c19bf
kiptoo haron <kiptooharon.hk@gmail.com>
2023-10-21 11:47:03 -0400

    monty opcodes

---
+++ rotl.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+/**
+* rotl - rotate elements of the stack
+* @stack: pointer to top of stack
+* @line_number: file line number of the opcode
+*/
+void rotl(stack_t **stack, unsigned int line_number)
+{
+	int tmp;
+
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%d: too short cant perform rotl\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*stack)->n;
+	while ((*stack)->next != NULL)
+	{
+		(*stack)->n = (*stack)->next->n;
+		*stack = (*stack)->next;
+	}
+	(*stack)->n = tmp;
+	while ((*stack)->prev != NULL)
+		*stack = (*stack)->prev;
+}
