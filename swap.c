eeb0102f5a530105835e83d456b45ea2bc0581a4
kiptoo haron <kiptooharon.hk@gmail.com>
2023-10-21 19:22:41 -0400

    update os swap opcode

--- swap.c
+++ swap.c
@@ -10,7 +10,7 @@ void swap(stack_t **stack, unsigned int line_number)
 
 	if ((*stack == NULL) || ((*stack)->next == NULL))
 	{
-		fprintf(stderr, "L%d: can't swap, stack too shorti\n", line_number);
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		free_exit(stack);
 		exit(EXIT_FAILURE);
 	}
