a14d4c8754df8088451a19a30c0ecda6f0f7f188
Anthony <ndegwaanthony300@gmail.com>
2023-10-22 11:29:46 -0700

    updates

--- Anthony/main.c
+++ Anthony/main.c
@@ -11,15 +11,6 @@ void exec_fxn(char *opcode, char *my_argument, unsigned int digit, stack_t **sta
 */
 int main(int argc, char **argv)
 {
-<<<<<<< HEAD
-    if (argc < 2)
-    {
-        fprintf(stderr, "Usage: %s filename\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-
-    FILE *my_file = fopen(argv[1], "r");
-=======
     char *my_opcode;
     char *my_arg;
     int iterator = 0;
@@ -33,50 +24,23 @@ int main(int argc, char **argv)
     }
 
     my_file = fopen(argv[1], "r");
->>>>>>> 47b6257474e52780db25bcc30e78717665c5eeb0
-
     if (my_file == NULL)
     {
         fprintf(stderr, "Error: Cannot open file %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
 
-<<<<<<< HEAD
-    char *my_opcode;
-    char *my_arg;
-    int iterator = 0;
-    ssize_t line_of_characters = 1;
-    char *storage = NULL;
-    size_t length_of_file = 0;
-
-    while (line_of_characters > 0)
-=======
     while (fgets(storage, sizeof(storage), my_file) != NULL)
->>>>>>> 47b6257474e52780db25bcc30e78717665c5eeb0
     {
         iterator++;
-<<<<<<< HEAD
-
-        if (line_of_characters > 0)
-        {
-            my_opcode = strtok(storage, "\t\n\r");
-            my_arg = strtok(NULL, "\t\n\r");
-
-            if (my_opcode != NULL)
-            {
-                exec_fxn(my_opcode, my_arg, iterator, &stack);
-            }
-        }
-    }
-=======
         my_opcode = strtok(storage, "\t\n\r");
         my_arg = strtok(NULL, "\t\n\r");
->>>>>>> 47b6257474e52780db25bcc30e78717665c5eeb0
 
         if (my_opcode != NULL)
         {
-            exec_fxn(my_opcode, my_arg, iterator, &stack);
+		exec_fxn(my_opcode, my_arg, iterator, &stack);
         }
+
     }
     
     fclose(my_file);
