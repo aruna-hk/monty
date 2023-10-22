1942b6b25b3cd28e1a8e9f55ff9d8c28829a1c21
kiptoo haron <kiptooharon.hk@gmail.com>
2023-10-22 07:10:35 -0400

    print character -opcode update

--- pchar.c
+++ pchar.c
@@ -19,6 +19,5 @@ void pchar(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 	fprintf(stdout, "%c", (*stack)->n);
-	if (C_FLAG == 0)
-		fprintf(stdout, "\n");
+	fprintf(stdout, "\n");
 }
